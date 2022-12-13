5f037b7fe7d1e9665d017df85675de56e002c898
Timo Sarkar <71646577+timosarkar@users.noreply.github.com>
2022-12-13 09:58:27 +0100

    adding base os

---
+++ build.sh
@@ -0,0 +1,24 @@
+set -e
+
+rm -rf .build
+mkdir .build
+cd .build
+
+BIN=mantrakernel
+curl -O https://github.com/mantraos/mantra/blob/main/.mantra/mantrakernel
+chmod +x $BIN
+
+mkdir out
+
+# workaround fs bug in HolyCRT (fails to create necessary directories)
+mkdir -p out/Compiler out/Kernel
+
+env STARTOS=Build/BuildMantra ./$BIN --drive=C,..,out
+
+ISO_FILE=Mantra.out/Mantra.ISO
+ISO_SIZE=$(wc -c <$ISO_FILE)
+MIN_SIZE=100000
+if [ $ISO_SIZE -le $MIN_SIZE ]; then
+    echo error: $ISO_FILE is $ISO_SIZE bytes in size, less than the expected minimum of $MIN_SIZE >&2
+    exit 1
+fi
